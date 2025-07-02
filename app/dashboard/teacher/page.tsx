a86a6166bba7cc89c3b0212ec1a391e12bbd543e
Aaron <aaronashby2006@gmail.com>
2025-07-02 13:17:24 -0400

    Add MUI dialog for creating courses on teacher dashboard
    Replaces the custom CreateButton with a Material UI dialog for adding new courses on the teacher dashboard. Adds @emotion/react and @emotion/styled as dependencies to support MUI components. Refactors state management and course creation logic to use the new dialog.

--- app/dashboard/teacher/page.tsx
+++ app/dashboard/teacher/page.tsx
@@ -1,23 +1,45 @@
-'use client'
+"use client";
 import { createClient } from "@/utils/supabase/client";
 
 import Navbar from "@/app/components/dashboard/Navbar";
-import CreateButton from "@/app/components/dashboard/CreateButton";
 import { useEffect, useState } from "react";
 import { Course, Profile } from "@/app/types";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import { SupabaseClient, User } from "@supabase/supabase-js";
 
 const TeacherPage = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [supabaseClient, setSupabaseClient] = useState<SupabaseClient>();
   const [profileData, setProfileData] = useState<Profile>();
   const [courseData, setCourseData] = useState<Course[] | null>([]);
+  const [createCourseModalOpen, setCreateCourseModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const supabase = createClient()
-      
-      const { data: { user } } = await supabase.auth.getUser()
+      const supabase = createClient();
+      setSupabaseClient(supabase);
 
-      const { data: profile } = await supabase.from('profiles').select('*').eq('id', user?.id).single()
-      const { data: courses } = await supabase.from('courses').select('*').eq('teacher', user?.id)
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+
+      const { data: profile } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", user?.id)
+        .single();
+      const { data: courses } = await supabase
+        .from("courses")
+        .select("*")
+        .eq("teacher", user?.id);
 
       if (!profile) {
         console.error("Profile record not found");
@@ -27,30 +49,74 @@ const TeacherPage = () => {
         first_name: profile?.first_name,
         last_name: profile?.last_name,
         username: profile?.username,
-      })
-      setCourseData(courses)
-    }
+      });
+      setCourseData(courses);
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  const openCreateCourseModal = () => {
-    console.log("Create course?")
-  }
+  const handleOpen = () => {
+    setCreateCourseModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setCreateCourseModalOpen(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const formJson = Object.fromEntries(formData.entries());
+    const name = formJson.courseName;
+
+    // TODO: Create method to generate random code
+    await supabaseClient
+      ?.from("courses")
+      .insert({ teacher: user?.id, name, code: "FOIDO56" });
+    handleClose();
+  };
 
   return (
     <div>
       <Navbar></Navbar>
       <div className="p-6">
-        <h1 className="text-2xl font-bold mb-4">
+        <h1 className="text-4xl font-bold mb-4">
           Hello, {profileData?.first_name}!
         </h1>
-        
+
         <div>
-            <div className="flex">
-                <h2 className="text-xl font-semibold mb-3">Your Courses</h2>
-                <CreateButton text="Add Course" onClick={openCreateCourseModal}></CreateButton>
+          <div className="flex mb-3">
+            <h2 className="text-2xl font-semibold pr-3">Your Courses</h2>
+            <div>
+              <Button variant="outlined" onClick={handleOpen}>
+                + Add Course
+              </Button>
+              <Dialog open={createCourseModalOpen} onClose={handleClose}>
+                <DialogTitle>Add New Course</DialogTitle>
+                <DialogContent>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      autoFocus
+                      required
+                      margin="dense"
+                      id="courseName"
+                      name="courseName"
+                      label="New course name"
+                      type="text"
+                      fullWidth
+                      variant="standard"
+                    ></TextField>
+                    <DialogActions>
+                      <Button onClick={handleClose}>Cancel</Button>
+                      <Button type="submit">Create course</Button>
+                    </DialogActions>
+                  </form>
+                </DialogContent>
+              </Dialog>
             </div>
+          </div>
           {courseData && courseData.length > 0 ? (
             <div className="grid gap-4">
               {courseData.map((course, index: number) => (
