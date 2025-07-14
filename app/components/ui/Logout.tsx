7b41ff31a714655bcab5ebca8a00dad3a36d66c8
Aaron <aaronashby2006@gmail.com>
2025-07-14 13:35:55 -0400

    Changed logout button styling

--- app/components/ui/Logout.tsx
+++ app/components/ui/Logout.tsx
@@ -4,7 +4,11 @@ import React from "react";
 
 const Logout = () => {
   return (
-    <Button onClick={logout} variant="outlined">
+    <Button
+      className="py-2 px-4 border-red-700 bg-transparent text-sm font-medium text-white transition hover:bg-red-400 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+      onClick={logout}
+      variant="outlined"
+    >
       Log Out
     </Button>
   );
